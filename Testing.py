e003d41fd7902eacfd7a39baa7693af8d6b077a6
JakeAtkinsonrhs <jakeatkinson@riccarton.school.nz>
2025-03-18 08:50:56 +1300

    cleared testing file

--- Testing.py
+++ Testing.py
@@ -1,22 +1,7 @@
 """this program will do stuff"""
 
 #variables
-MINIMUM = 34
-MAXIMUM = 42
-temperatures = []
-end = -1
-variable_idk = int(input('Enter the temperature: '))
 
 #loops and other code
-while variable_idk != end:
-  temperatures.append(variable_idk)
-  variable_idk = int(input('Enter the temperature: '))
 
 #final printing
-for temps in temperatures:
-  if temps < MINIMUM:
-    print("too cold")
-  elif temps > MAXIMUM:
-    print("too hot")
-  else:
-    print("just right")
