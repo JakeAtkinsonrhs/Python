74ce06d386cf883a636eaae60d61a671af2577ca
JakeAtkinsonrhs <jakeatkinson@riccarton.school.nz>
2025-03-25 08:50:38 +1300

    added variables and started loops

--- assesmentcodewriting.py
+++ assesmentcodewriting.py
@@ -1,7 +1,16 @@
-"""this program will do ___"""
+"""this program will detect whether certain speeds are unsafe"""
 
-#variables, lists, and dictionaries
+#variables
+SAFE_SPEED = 10.0
+FINISH_WORD = 'terminate'
+
+#make an empty list to store speeds
+safe_speeds = []
+unsafe_speeds = []
 
 #loops and other code (add notation as needed)
+while True:
+    try:
+        
 
 #final printing / code ending
